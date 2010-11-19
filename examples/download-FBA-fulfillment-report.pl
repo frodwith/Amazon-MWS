be8c4fce74462b20998b1163b76e3ca7ec1800a6
Pete Kruckenberg <pete@kruckenberg.com>
2010-11-19 10:27:51 -0700

    Amazon::MWS examples

---
+++ examples/download-FBA-fulfillment-report.pl
@@ -0,0 +1,28 @@
+# Download completed FBA fulfillment report
+
+use strict;
+
+use Amazon::MWS::Client;
+
+opendir(my $dir, ".");
+my @requests = map { /(\d+)/ } grep { /^request\.\d+$/ && -f $_ } readdir($dir);
+closedir $dir;
+
+exit unless @requests;
+
+my $mws = Amazon::MWS::Client->new(access_key_id=>"XXX",
+                                   secret_key => "YYY",
+                                   merchant_id => "ZZZ",
+                                   marketplace_id => "VVV");
+
+for my $req (@{$mws->GetReportRequestList(ReportRequestIdList => \@requests)->{ReportRequestInfo}}) {
+    if ($req->{ReportProcessingStatus} eq '_DONE_' && (my $report_id = $req->{GeneratedReportId})) {
+        my $report = $mws->GetReport(ReportId => $report_id);
+        if (length($report)) {
+            open my $file, "> report.${report_id}";
+            print $file $report;
+            close $file;
+            unlink "request.$req->{ReportRequestId}";
+        }
+    }
+}
---
+++ examples/request-FBA-fulfillment-report.pl
@@ -0,0 +1,20 @@
+# Initiate FBA fulfillment report generation
+
+use strict;
+
+use Amazon::MWS::Client;
+use DateTime;
+
+my $mws = Amazon::MWS::Client->new(access_key_id=>"XXX",
+                                   secret_key => "YYY",
+                                   merchant_id => "ZZZ",
+                                   marketplace_id => "VVV");
+
+my $req = $mws->RequestReport(ReportType => '_GET_AMAZON_FULFILLED_SHIPMENTS_DATA_',
+                              StartDate => DateTime->now->add(weeks => -1),
+                              EndDate => DateTime->now);
+
+if (my $req_id = $req->{ReportRequestInfo}->[0]->{ReportRequestId}) {
+    open my $req, "> request.${req_id}";
+    close $req;
+}
